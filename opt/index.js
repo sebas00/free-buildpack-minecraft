7ec35dfb570b7db1072da0a3304aa52c81227213
jdtzmn <jdtzmn@gmail.com>
2017-10-30 16:38:44 -0400

    changed index.js

--- opt/index.js
+++ opt/index.js
@@ -1,26 +1,35 @@
 #!/usr/bin/env nodejs
 
-var net = require('net')
 var fs = require('fs')
-var Socket = net.Socket
-var Server = net.Server
+var http = require('http')
 
 // Get port of ngrok from ngrok.log
 var log = fs.readFileSync('ngrok.log', {encoding: 'utf-8'})
 var regex = /URL:tcp:\/\/0.tcp.ngrok.io:(\d{5})/g
 var port = regex.exec(log)[1]
 
-// Start server to forward to the ngrok minecraft server
-var server = new Server()
-server.on('connection', function (client) {
-  console.log('New server connection')
-  var socket = new Socket()
-  console.log('Connecting to 0.tcp.ngrok.io:' + port)
-  socket.connect(port, '0.tcp.ngrok.io', function () {
-    console.log('Server connection successful')
-    socket.pipe(client)
-    client.pipe(socket)
-  })
-})
+http.createServer(function (request, response) {
+  response.writeHead(200, { 'Content-Type': 'application/json' })
+  response.end('Server Address: 0.tcp.ngrok.io:' + port)
+}).listen(process.env.PORT || 8080)
 
-server.listen(process.env.PORT || 25565)
+/*
+    The *old* client side code to forward a server to localhost
+*/
+
+// Start server to forward to the ngrok minecraft server
+// var net = require('net')
+// var Socket = net.Socket
+// var Server = net.Server
+// var server = new Server()
+// server.on('connection', function (client) {
+//   console.log('New server connection')
+//   var socket = new Socket()
+//   console.log('Connecting to 0.tcp.ngrok.io:' + port)
+//   socket.connect(port, '0.tcp.ngrok.io', function () {
+//     console.log('Server connection successful')
+//     socket.pipe(client)
+//     client.pipe(socket)
+//   })
+// })
+// server.listen(process.env.PORT || 25565)
