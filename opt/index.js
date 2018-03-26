82a72087baaa9bce68464faa3f8dd22576f27ae9
jdtzmn <jdtzmn@gmail.com>
2018-03-25 20:56:30 -0400

    cleanup

--- opt/index.js
+++ opt/index.js
@@ -12,24 +12,3 @@ http.createServer(function (request, response) {
   response.writeHead(200, { 'Content-Type': 'application/json' })
   response.end('Server Address: 0.tcp.ngrok.io:' + port)
 }).listen(process.env.PORT || 8080)
-
-/*
-    The *old* client side code to forward a server to localhost
-*/
-
-// Start server to forward to the ngrok minecraft server
-// var net = require('net')
-// var Socket = net.Socket
-// var Server = net.Server
-// var server = new Server()
-// server.on('connection', function (client) {
-//   console.log('New server connection')
-//   var socket = new Socket()
-//   console.log('Connecting to 0.tcp.ngrok.io:' + port)
-//   socket.connect(port, '0.tcp.ngrok.io', function () {
-//     console.log('Server connection successful')
-//     socket.pipe(client)
-//     client.pipe(socket)
-//   })
-// })
-// server.listen(process.env.PORT || 25565)
